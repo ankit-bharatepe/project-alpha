55193ae2defcb1255b2f38661028e16b9c9cc8bc
Anamika <admin@Apples-MacBook-Pro.local>
2021-03-05 17:35:06 +0530

    jinny done

--- src/main/java/com/bharatpe/projectalpha/projectalpha/controller/TransactionController.java
+++ src/main/java/com/bharatpe/projectalpha/projectalpha/controller/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +15,8 @@ public class TransactionController {
     @Autowired
     TransactionManage transactionManage;
     @RequestMapping(value = "/get-lucky-transaction", method = RequestMethod.GET)
-    public ResponseEntity<Object> getLuckyTransaction() throws JsonProcessingException {
-        return transactionManage.getTransaction();
+    public ResponseEntity<Object> getLuckyTransaction(@RequestParam(name = "amount") String amount , @RequestParam(name = "name") String name) throws JsonProcessingException {
+        return transactionManage.getTransaction(amount,name);
 
     }
 }
