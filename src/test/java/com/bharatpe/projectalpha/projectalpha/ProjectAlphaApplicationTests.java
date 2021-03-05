911f58c18d7ad23cbb5a5e88f66e49ea7282cca1
ankit <ankit.mishra@bharatpe.com>
2021-03-05 14:06:20 +0530

    initial commit

---
+++ src/test/java/com/bharatpe/projectalpha/projectalpha/ProjectAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharatpe.projectalpha.projectalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
