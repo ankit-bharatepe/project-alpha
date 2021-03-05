98548dc0e7e58b9a96d5554d0566015d7b4199cc
ankit <ankit.mishra@bharatpe.com>
2021-03-05 22:06:28 +0530

    querry

--- src/main/java/com/bharatpe/projectalpha/projectalpha/dao/ExpenseLedgerDao.java
+++ src/main/java/com/bharatpe/projectalpha/projectalpha/dao/ExpenseLedgerDao.java
@@ -3,7 +3,11 @@ package com.bharatpe.projectalpha.projectalpha.dao;
 import com.bharatpe.projectalpha.projectalpha.entity.ExpenseLedger;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ExpenseLedgerDao extends CrudRepository<ExpenseLedger, Long> {
 
+    List<ExpenseLedger> findBYMerchantId(Long merchantId);
 
+    List<ExpenseLedger> findByMerchantIdAndMerchantExpenseAggId(Long merchantId, Long merchantExpenseAggId);
 }
